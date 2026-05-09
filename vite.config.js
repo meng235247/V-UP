93145ca70adbf8df6a771f3f8e0588a8b523991f
meng <269193914+meng235247@users.noreply.github.com>
2026-05-09 15:07:12 +0800

    fix(build): add dashboard.js to viteStaticCopy targets to fix UI interactions on Netlify

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,10 @@ export default defineConfig({
           src: 'image',
           dest: '',
         },
+        {
+          src: 'dashboard.js',
+          dest: '',
+        },
       ],
     }),
   ],
